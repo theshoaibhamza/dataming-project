9a47976b11de493a004c89be0adb3f2343e33295
Chaudhary Shoaib Hamza <bigbrains997@gmail.com>
2025-06-21 02:40:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ def predict():
         X_scaled = scaler.transform(df.astype(float))
         g3 = rf.predict(X_scaled)[0]
         return jsonify({
-            "predicted_g3": round(float(g3), 2),
-            "grade": grade_letter(g3),
-            "passed": g3 >= 10
+        "predicted_g3": round(float(g3), 2),
+        "grade": grade_letter(g3),
+        "passed": bool(g3 >= 10)
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 400
